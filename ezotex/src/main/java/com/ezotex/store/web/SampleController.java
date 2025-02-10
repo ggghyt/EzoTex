9dc634813ed1cc756c407a03cc811e8441e14076
leejh1129 <leejh09876@naver.com>
2025-02-10 23:55:53 +0900

    추가: 사이드바 링크, 링크별 페이지 자재/제품 입고 등록 페이지 레이아웃

--- ezotex/src/main/java/com/ezotex/store/web/SampleController.java
+++ ezotex/src/main/java/com/ezotex/store/web/SampleController.java
@@ -1,16 +1,75 @@
 package com.ezotex.store.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-//@Controller
+@Controller
 @Slf4j
-// @RequiredArgsConstructor
-// @RequestMapping("/내부주소/*")
+@RequiredArgsConstructor
+@RequestMapping("/store/*")
 public class SampleController {
+
+	@GetMapping("test")
+	public String index() {
+		return "store/sample";
+	}
+
+	/**
+	 * ========================================= 
+	 * 입고관련 컨트롤
+	 * =========================================
+	 */
+
+	// 자재/제품 입고 등록 페이지
+	@GetMapping("insertStore")
+	public String storeInsert() {
+		return "store/insertStore";
+	}
+
+	// 반품 입고 등록 페이지
+	@GetMapping("insertReturnStore")
+	public String storeList() {
+		return "store/insertReturnStore";
+	}
+
+	// 입고 문서 관리 페이지
+	@GetMapping("docManagement")
+	public String docManagement() {
+		return "store/docManagement";
+	}
+
+	/**
+	 * ========================================= 
+	 * 자재관련 컨트롤
+	 * =========================================
+	 */
+
+	// 재고 조회 페이지
+	@GetMapping("productList")
+	public String productList() {
+		return "store/productList";
+	}
 	
-//	@GetMapping("/")
-//	public String index() {
-//		return "index";
-//	}
-	
+	// 재고 조정 페이지
+	@GetMapping("productManagement")
+	public String productManagement() {
+		return "store/productManagement";
+	}
+		
+	// 실시간 재고 모니터링 페이지
+	@GetMapping("monitoring")
+	public String monitoring() {
+		return "store/monitoring";
+	}
+		
+	// 불량처리 조회 페이지
+	@GetMapping("errorList")
+	public String errorList() {
+		return "store/errorList";
+	}
+
 }
