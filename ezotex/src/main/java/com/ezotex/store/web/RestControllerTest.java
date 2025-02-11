815fce96fabae71a6f7a68e0bd519af84ac84c56
leejh1129 <leejh09876@naver.com>
2025-02-11 18:01:14 +0900

    추가 : 토스트 그리드 적용

---
+++ ezotex/src/main/java/com/ezotex/store/web/RestControllerTest.java
@@ -0,0 +1,30 @@
+package com.ezotex.store.web;
+
+import java.util.List;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ezotex.store.dto.InventoryDTO;
+import com.ezotex.store.service.InventoryService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/store/*")
+@RestController
+public class RestControllerTest {
+
+	private final InventoryService service;
+	
+	@GetMapping("test2")
+	public List<InventoryDTO> test() {
+		return service.list();
+	}
+
+	
+}
