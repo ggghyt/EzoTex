7473bde3c67750f7ad2474bee324d64398336ac2
Taebaek Chio <112chamchi@naver.com>
2025-02-11 16:28:33 +0900

    태백 긴급수정

--- ezotex/src/main/java/com/ezotex/delivery/mappers/DeliveryMapper.java
+++ ezotex/src/main/java/com/ezotex/delivery/mappers/DeliveryMapper.java
@@ -2,6 +2,9 @@ package com.ezotex.delivery.mappers;
 
 import java.util.List;
 
+import com.ezotex.delivery.dto.RegistDeliveryDTO;
+
 public interface DeliveryMapper {
 	
+	List<RegistDeliveryDTO> findAll();
 }
