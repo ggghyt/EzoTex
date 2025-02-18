b6c6b713dd3164c9bc4bdcd123bccd46a2692922
Taebaek Chio <112chamchi@naver.com>
2025-02-18 15:25:35 +0900

    수정: 페이징DTO

--- ezotex/src/main/java/com/ezotex/comm/dto/PagingDTO.java
+++ ezotex/src/main/java/com/ezotex/comm/dto/PagingDTO.java
@@ -8,12 +8,12 @@ public class PagingDTO {
 	int pageUnit=10 ;  //한페이지 출력할 레코드 건수
 	int pageSize=10 ;  //페이지번호 수 (5) 1~ 5까지
 	int lastPage;      //마지막 페이지번호
-	int totalRecord;  //전체 레코드건수
-	Integer page;	  //현재 페이지
-	int startPage;  //페이지그룹내에서 시작페이지번호
-	int endPage;  //페이지그룹내에서 마지막페이지번호
-	int first;
-	int last;
+	int totalRecord;   //전체 레코드건수
+	Integer page;	   //현재 페이지
+	int startPage;     //페이지그룹내에서 시작페이지번호
+	int endPage;       //페이지그룹내에서 마지막페이지번호
+	int first;		   //시작페이지
+	int last;		   //마지막페이지
 
 	public int getFirst() {
 		first = (getPage() - 1) * getPageUnit() + 1;
