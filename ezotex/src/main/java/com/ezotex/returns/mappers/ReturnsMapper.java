be0b3ec4903003b2a27140fc2d1efcbcb95bc50a
SMGyo <alsryaks1@naver.com>
2025-02-25 23:55:19 +0900

    추가 : 반품 등록 (제품 등록 오류수정 해야함)

--- ezotex/src/main/java/com/ezotex/returns/mappers/ReturnsMapper.java
+++ ezotex/src/main/java/com/ezotex/returns/mappers/ReturnsMapper.java
@@ -4,11 +4,20 @@ import java.util.List;
 
 import com.ezotex.returns.dto.DeliveryDetailsReturnsDTO;
 import com.ezotex.returns.dto.DeliveryReturnsDTO;
+import com.ezotex.returns.dto.ReturnsDTO;
+import com.ezotex.returns.dto.ReturnsProductDTO;
 
 public interface ReturnsMapper {
-	// 출고 내역 조회
+	// 납품 내역 조회
 	List<DeliveryReturnsDTO> getDeliveryList();
-
+	
+	// 납품 상세 조회
 	List<DeliveryDetailsReturnsDTO> getDeliProduct(String deliveryCode);
+	
+	// 반품 헤더 등록
+	int insertReturn(ReturnsDTO returnData);
+	
+	// 반품 제품 등록
+	int insertProductReturn(List<ReturnsProductDTO> returnProductData);
 	
 }
--- ezotex/src/main/java/com/ezotex/returns/service/ReturnsService.java
+++ ezotex/src/main/java/com/ezotex/returns/service/ReturnsService.java
@@ -4,9 +4,19 @@ import java.util.List;
 
 import com.ezotex.returns.dto.DeliveryDetailsReturnsDTO;
 import com.ezotex.returns.dto.DeliveryReturnsDTO;
+import com.ezotex.returns.dto.ReturnsDTO;
+import com.ezotex.returns.dto.ReturnsProductDTO;
 
 public interface ReturnsService {
+	// 납품 내역 조회
 	List<DeliveryReturnsDTO> getDeliveryList();
 	
+	// 납품 상세 조회
 	List<DeliveryDetailsReturnsDTO> getDeliProduct(String deliveryCode);
+	
+	// 반품 헤더 등록
+	ReturnsDTO insertReturn(ReturnsDTO returnData);
+	
+	// 반품 제품 등록
+	List<ReturnsProductDTO> insertProductReturn(List<ReturnsProductDTO> returnProductData);
 }
