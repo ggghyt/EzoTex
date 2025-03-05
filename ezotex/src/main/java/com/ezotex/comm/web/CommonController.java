9b8f57880aedde9af89c4256bc4cb8329784947d
leejh1129 <leejh09876@naver.com>
2025-03-05 12:46:05 +0900

    수정 : url localhost 제거

--- ezotex/src/main/java/com/ezotex/comm/web/CommonController.java
+++ ezotex/src/main/java/com/ezotex/comm/web/CommonController.java
@@ -50,25 +50,25 @@ public class CommonController {
     // 로그인 메인 페이지
 	@GetMapping("/main")
 	public String login() {
-		return "/login/login";
+		return "login/login";
 	}
 
     // 회원가입 메인 페이지
 	@GetMapping("/register_main")
 	public String register_main() {
-		return "/login/register_main";
+		return "login/register_main";
 	}
 
     // 공급업체 회원가입 페이지
 	@GetMapping("/register_supply")
 	public String register_supply() {
-		return "/login/register_supply";
+		return "login/register_supply";
 	}
 
 	// 공급업체 회원가입 페이지
 	@GetMapping("/register_emp")
 	public String register_emp() {
-		return "/login/register_emp";
+		return "login/register_emp";
 	}
 	
 	// 로그인 중복 확인 메세지
